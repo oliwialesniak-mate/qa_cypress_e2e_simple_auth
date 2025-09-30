c4fad0f8510246f20b98eb4cbadbe7931b273034
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-30 17:21:07 +0200

    Add Cypress tests for login and logout

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,44 +2,37 @@
 
 describe('Login Flow Tests with Custom Commands', () => {
   beforeEach(() => {
-    cy.visit('/login');
+    cy.visit('/login'); // Single navigation for all tests
   });
 
   it('Login with valid credentials', () => {
     cy.login();
-
     cy.url().should('include', '/secure');
     cy.get('h2').should('contain.text', 'Secure Area');
-    cy.get('#flash')
-      .should('be.visible')
+    cy.get('#flash').should('be.visible')
       .and('contain.text', 'You logged into a secure area!');
   });
 
   it('Login with invalid username and valid password', () => {
     cy.login('wrongUser', 'SuperSecretPassword!');
-
     cy.location('pathname').should('eq', '/login');
-    cy.get('#flash')
-      .should('be.visible')
+    cy.get('#flash').should('be.visible')
       .and('contain.text', 'Your username is invalid!');
   });
 
   it('Login with valid username and invalid password', () => {
     cy.login('tomsmith', 'wrongPassword');
-
     cy.location('pathname').should('eq', '/login');
-    cy.get('#flash')
-      .should('be.visible')
+    cy.get('#flash').should('be.visible')
       .and('contain.text', 'Your password is invalid!');
   });
 
   it('Logout from the app', () => {
     cy.login();
     cy.logout();
-
     cy.location('pathname').should('eq', '/login');
-    cy.get('#flash')
-      .should('be.visible')
+    cy.get('#username').should('be.visible'); // Assert login form visible
+    cy.get('#flash').should('be.visible')
       .and('contain.text', 'You logged out of the secure area!');
   });
 });
