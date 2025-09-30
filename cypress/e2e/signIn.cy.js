7f29f922e1fc8f7210bea70ae3bc308a7b80bae9
Oliwia Lesniak <oliwialesniak666@gmail.com>
2025-09-30 15:03:14 +0200

    Add Cypress tests for login and logout

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,26 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+describe('Login Flow Tests with Custom Commands', () => {
+  it('Login with valid credentials', () => {
+    cy.login(); // Default valid credentials
 
+    cy.url().should('include', '/secure');
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!');
   });
 
-  it('', () => {
+  it('Login with invalid credentials', () => {
+    cy.login('invalidUsername', 'invalidPassword');
 
+    cy.url().should('eq', 'https://the-internet.herokuapp.com/login');
+    cy.get('#flash').should('contain.text', 'Your username is invalid!');
+  });
+
+  it('Logout from the app', () => {
+    cy.login(); // Login first
+    cy.logout(); // Then logout
+
+    cy.url().should('eq', 'https://the-internet.herokuapp.com/login');
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
